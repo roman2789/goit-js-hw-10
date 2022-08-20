4132c387247a84eea5bcf7c137f31cf5ccd5776b
Roman Andrukhiv <andrukhivroman@gmail.com>
2022-08-20 14:12:32 +0300

    first commit

---
+++ src/fetchCountries.js
@@ -0,0 +1,14 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name';
+const searchFields = new URLSearchParams({
+  fields: ['name', 'capital', 'population', 'flags', 'languages'],
+});
+export function fetchCountries(name) {
+  return fetch(`${BASE_URL}/${name}?${searchFields}`)
+    .then(response => {
+      if (response.status === 404) {
+        throw new Error(response.status);
+      }
+      return response.json;
+    })
+    .catch(console.log('Error!'));
+}
