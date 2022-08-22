241f3b042e8aea78801ca3377158848a741626b4
Roman Andrukhiv <andrukhivroman@gmail.com>
2022-08-22 19:21:53 +0300

    add

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,21 +1,3 @@
-// const BASE_URL = 'https://restcountries.com/v3.1/all';
-// const searchFields = new URLSearchParams({
-//   fields: ['name', 'capital', 'population', 'flags', 'languages'],
-// });
-
-// export function fetchCountries(name) {
-//   return fetch(`${BASE_URL}/${name}?${searchFields}`)
-//     .then(response => {
-//       // console.log(response);
-//       if (response.status === 404) {
-//         throw new Error(response.status);
-//       }
-
-//       return response.json();
-//     })
-//     .catch(console.log('Error!'));
-// }
-
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
 const fields = 'fields = name,capital,population,flags,languages';
 
@@ -24,16 +6,3 @@ export function fetchCountries(name) {
     .then(response => response.json())
     .catch(error => console.log(error));
 }
-
-// export function fetchCountries(name) {
-//   return fetch(`${BASE_URL}/${name}?${searchFields}`)
-//     .then(response => {
-//       // console.log(response);
-//       if (response.status === 404) {
-//         throw new Error(response.status);
-//       }
-
-//       return response.json();
-//     })
-//     .catch(console.log('Error!'));
-// }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-// import './css/styles.css';
-// import { fetchCountries } from './fetchCountries';
-
-// const DEBOUNCE_DELAY = 300;
-
-// console.log(fetchCountries());
-
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
